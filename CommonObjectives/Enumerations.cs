3528995b2331eb81a20bc5ddaf70e80ef3e392b6
Slothie <dallas.adams@outlook.com>
2021-06-16 11:51:37 +1000

    Added References.

--- CommonObjectives/Enumerations.cs
+++ CommonObjectives/Enumerations.cs
@@ -1,21 +1,14 @@
 ﻿namespace CommonObjectives
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-
-    class Enumerations
+    public enum ApplicationType
     {
-        public enum ApplicationType
-        {
-            None = 0,
-            VisualStudio = 1,
-            SSMS = 2,
-            Word = 3,
-            Excel = 4,
-            AutoCAD = 5,
-        }
+        None = 0,
+        VisualStudio = 1,
+        SSMS = 2,
+        Word = 3,
+        Excel = 4,
+        AutoCAD = 5,
     }
+
+
 }
--- WindowsObjectives/FormMain.Designer.cs
+++ WindowsObjectives/FormMain.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace WindowsObjectives
 {
-    partial class Form1
+    partial class FormMain
     {
         /// <summary>
         /// Required designer variable.
